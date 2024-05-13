7bb92305cb8e1069968700cd536f3ee2c559a031
vcarvalho0 <vitor1.santos00@gmail.com>
2024-05-13 18:47:59 -0300

    feat: add path alias

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,9 @@
+import Container from '@/components/container'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="flex flex-row h-[100vh] justify-between">
+      <Container>Hello World</Container>
+    </div>
+  )
 }
