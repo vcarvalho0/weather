ff9f4a61dfabee12fbd6ace94846b9f1d6d1a6b0
vcarvalho0 <vitor1.santos00@gmail.com>
2024-06-03 21:02:38 -0300

    chore: fix storybook next.js

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from '@storybook/react-vite'
+import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -7,11 +7,12 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: '@storybook/react-vite',
-    options: {}
-  }
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
 }
 export default config
