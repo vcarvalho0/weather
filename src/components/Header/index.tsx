04ec5c4f25ec5f80264c340eeffa2b2ba2a5f0d3
vcarvalho0 <vitor1.santos00@gmail.com>
2024-06-03 21:06:45 -0300

    chore: refactor file structure and add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const commonClass = 'text-lg transition-opacity hover:opacity-70 border-b-2'
+const linkClass = `${commonClass} border-transparent hover:border-blue-200`
+const activateLinkClass = `${commonClass} border-blue-500`
+
+export const Header = () => {
+  const pathname = usePathname()
+
+  return (
+    <header className="item-center container mx-auto flex justify-between px-4 py-8">
+      <nav className="flex gap-6">
+        <Link
+          className={pathname === '/' ? activateLinkClass : linkClass}
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className={pathname === '/' ? activateLinkClass : linkClass}
+          href="/"
+        >
+          Home
+        </Link>
+      </nav>
+    </header>
+  )
+}
