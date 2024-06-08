5f3dca6af5ba4c3201aa0965243b2bc48b32a1c6
vcarvalho0 <vitor1.santos00@gmail.com>
2024-06-08 20:13:03 -0300

    feat: add Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="container mx-auto px-4 pb-8 pt-16 text-center">
+      <p className="flex flex-col justify-center gap-2 sm:flex-row">
+        <span>Created with Next.js</span>
+      </p>
+    </footer>
+  )
+}
